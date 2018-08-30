8e82d4aa33f9fa5f05465eee6b051297a34fa755
Jae Choi <jaechoi@Jae-MacBook-Pro.local>
2018-08-30 17:36:45 +0900

    underbar-uniq function complete

--- underbar-uniq/underbar-uniq.js
+++ underbar-uniq/underbar-uniq.js
@@ -5,6 +5,16 @@ var _ = {};
 
   // Produce a duplicate-free version of the array.
   _.uniq = function(array) {
+    var uniqueArray = [];
+
+    array = array.sort();
+    for (var i = 0; i < array.length; i++) {
+      if (array[i] !== array[i+1]) {
+        uniqueArray.push(array[i]);
+      }
+    }
+
+    return uniqueArray;
   };
 
 }).call(this);
