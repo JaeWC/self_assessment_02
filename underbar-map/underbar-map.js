c725b8114db69447859ea30688e64573c5d90e0c
Jae Choi <jaechoi@Jae-MacBook-Pro.local>
2018-08-30 17:31:48 +0900

    underbar-map function complete

--- underbar-map/underbar-map.js
+++ underbar-map/underbar-map.js
@@ -8,6 +8,19 @@ var _ = {};
     // map() is a useful primitive iteration function that works a lot
     // like each(), but in addition to running the operation on all
     // the members, it also maintains an array of results.
+    var mappedArray = [];
+
+    if (Array.isArray(collection)) {
+      for (var i = 0; i < collection.length; i++) {
+        mappedArray.push(iterator(collection[i]));
+      }
+    } else {
+      for (var prop in collection) {
+        mappedArray.push(iterator(collection[prop]));
+      }
+    }
+
+    return mappedArray;
   };
 
 }).call(this);
